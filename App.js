b015f17686876bad262d8a5deff1422f07841a26
Ömer Faruk Ayyıldız <omerfaruk914@gmail.com>
2025-07-26 18:28:26 +0300

    Splitting component

--- App.js
+++ App.js
@@ -1,45 +1,28 @@
 import {useState} from 'react'
 import {
-    Button,
     StyleSheet,
-    Text,
     View,
-    TextInput,
-    ScrollView,
     Keyboard,
     TouchableWithoutFeedback,
     FlatList
 } from 'react-native';
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-    const [enteredGoalText, setEnteredGoalText] = useState('');
     const [courseGoals, setCourseGoals] = useState([])
 
-    function goalInputHandler(enteredText) {
-        //console.log(enteredText);
-        setEnteredGoalText(enteredText);
-    }
-
-    function addGoalHandler() {
-        setCourseGoals(currentCourseGoals => [...courseGoals, {text: enteredGoalText, id: Math.random().toString()}])
+    function addGoalHandler(enteredGoalText) {
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString()}])
     }
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <View style={styles.appContainer}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.textInput}
-                        placeholder={'Your Course Goal'}
-                        onChangeText={goalInputHandler}
-                    />
-                    <Button title="Add Goal" onPress={addGoalHandler}/>
-                </View>
+                <GoalInput onAddGoal={addGoalHandler} />
                 <View style={styles.goalsContainer}>
                     <FlatList data={courseGoals} renderItem={(itemData) => {
-                        return (
-                            <Text style={styles.goalItem}>{itemData.item.text}</Text>
-                        );
+                        return <GoalItem text={itemData.item.text} />;
                     }}
                               keyExtractor={(item, index) => {
                                   return item.id;
@@ -56,30 +39,9 @@ const styles = StyleSheet.create({
         paddingTop: 50,
         paddingHorizontal: 16,
     },
-    inputContainer: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        width: '70%',
-        marginRight: 8,
-        padding: 8
-    },
+
+
     goalsContainer: {
         flex: 4,
     },
-    goalItem: {
-        margin: 8,
-        padding: 8,
-        borderRadius: 6,
-        backgroundColor: '#5e0acc',
-        color: 'white'
-    }
 });
